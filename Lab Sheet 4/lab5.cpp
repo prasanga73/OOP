75030912e51c09d754baafdc08f43a9a580d3ebe
Prasanga Niraula <139893953+prasanga73@users.noreply.github.com>
2023-07-19 15:05:50 +0545

    Update lab5.cpp

--- Lab Sheet 4/lab5.cpp
+++ Lab Sheet 4/lab5.cpp
@@ -1,3 +1,13 @@
+// Write a program that illustrates the following relationship and comment on them. 
+
+// i) const_object.non_const_mem_function
+
+// ii) const_object.const_mem_function
+
+// iii) non_const_object.non_const_mem_function
+
+
+
 #include <iostream>
 using namespace std;
 
@@ -14,9 +24,9 @@ public:
 int main()
 {
     Relation r(1);
-    cout << "non const object calling non const mem function = " << r.get() << endl;  // non const object. non const mem func
-    cout << "non const object calling const mem func = " << r.const_get() << endl; // non const object. const mem
+    cout << "Non constant object calling non constant member function = " << r.get() << endl;  // non const object. non const mem func
+    cout << "Non constant object calling constant member function = " << r.const_get() << endl; // non const object. const mem
     const Relation c(2);
-    cout << "const object calling const mem function = " << c.const_get(); // const object.cont mem func
-    // cout << c.get();  cannot be called
+    cout << "Constant object calling constant member function = " << c.const_get(); // const object.cont mem func
+    // cout << c.get();  cannot be called because constant object cannot call non constant member function
 }
