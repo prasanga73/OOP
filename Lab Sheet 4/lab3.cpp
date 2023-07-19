d0a1953c9d1b4174d53072f750d15d009e425e36
Prasanga Niraula <139893953+prasanga73@users.noreply.github.com>
2023-07-19 15:01:46 +0545

    Update lab3.cpp

--- Lab Sheet 4/lab3.cpp
+++ Lab Sheet 4/lab3.cpp
@@ -1,3 +1,8 @@
+// Write a class that can store Department ID and Department Name with constructors 
+// to initialize its members. Write destructor member in the same class and 
+// display the message "Object n goes out of the scope". Your program should
+// be made such that it should show the order of constructor and destructor invocation.
+
 #include <iostream>
 using namespace std;
 
