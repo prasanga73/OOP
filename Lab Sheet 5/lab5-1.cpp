02ba0b75fa264513d67d10bff1875e97c49e31c5
Prasanga Niraula <078bct056@ioepc.edu.np>
2023-07-25 21:38:47 +0545

    edited answer

--- Lab Sheet 5/lab5-1.cpp
+++ Lab Sheet 5/lab5-1.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 using namespace std;
+class another;
+class cartesian{
+    int x,y;
+    public:
+    cartesian (int a,int b){
+        x=a;y=b;
+    }
+    friend int sum(cartesian,another);
+
+};
+class another{
+    int x,y;
+    public:
+    another (){}
+    another (int a,int b){
+        x=a;y=b;
+    }
+
+    friend int sum(cartesian,another);
+};
+int sum(cartesian c,another a){
+    c.x=c.x+a.x;
+    c.y=c.y+a.y;
+    cout<<"The sum of the co-ordinates are: "<<c.x<<","<<c.y;
+
+}
 
 
 int main(){
-    cout<<"Hello";
+    cartesian c(10,5);
+    another a(12,5);
+    another b;
+    sum(c,a); 
     
     return 0;
 }
